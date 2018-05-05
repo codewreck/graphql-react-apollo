8ad96333a545e56c5f9eb2a57d84299dc37b626c
Kunal Agarwal <kunal94agarwal@gmail.com>
2018-05-05 06:47:08 +0530

    Mutations: Creating Links

---
+++ src/components/CreateLink.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+class CreateLink extends React.Component {
+  state = {
+    description: '',
+    url: '',
+  }
+  render(){
+    return (
+      <div>
+        <div>
+          <input
+            value={this.state.description}
+            onChange={(e) => this.setState({ description: e.target.value })}
+            type="text"
+            placeholder="A description for link"
+          />
+          <input
+            value={this.state.url}
+            onChange={(e) => this.setState({ url: e.target.value })}
+            type="text"
+            placeholder="A url for link"
+          />
+        </div>
+        <button onClick={() => this._createLink()}>Submit</button>
+      </div>
+    )
+  }
+  _createLink = async () => {
+  const { description, url } = this.state
+  await this.props.postMutation({
+    variables: {
+      description,
+      url
+    }
+  })
+}
+}
+
+const POST_MUTATION = gql`
+  # 2
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+export default graphql(POST_MUTATION, { name: 'postMutation' })(CreateLink)
